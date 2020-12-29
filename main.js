5805493c786473b8b128deaac77600c26372f0b8
tevanyanmartin <74008842+tevanyanmartin@users.noreply.github.com>
2020-12-30 00:56:10 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-$(document).ready(function(){
-    $('.open-menu').click(function (){
-        $(this).css('display','none')
-        $('.close-menu').css('display','block')
-        $('.home').css('right','0')
-        $('body').css('overflow','hidden')
+$(document).ready(function () {
+    if ($(window).width() <= 500) {
 
-    })
-    $('.close-menu').click(function (){
-        $(this).css('display','none')
-        $('.open-menu').css('display','block')
-        $('.home').css('right','-110%')
-        $('body').css('overflow','auto')
-    })
-    $('.close-menu , .menu a').click(function (){
-        $('.close-menu').css('display','none')
-        $('.open-menu').css('display','block')
-        $('.home').css('right','-110%')
-        $('body').css('overflow','auto')
+
+        $('.open-menu').click(function () {
+            $(this).css('display', 'none')
+            $('.close-menu').css('display', 'block')
+            $('.home').css('right', '0')
+            $('body').css('overflow', 'hidden')
+
+        })
+        $('.close-menu').click(function () {
+            $(this).css('display', 'none')
+            $('.open-menu').css('display', 'block')
+            $('.home').css('right', '-110%')
+            $('body').css('overflow', 'auto')
+        })
+        $('.close-menu , .menu a').click(function () {
+            $('.close-menu').css('display', 'none')
+            $('.open-menu').css('display', 'block')
+            $('.home').css('right', '-110%')
+            $('body').css('overflow', 'auto')
+        })
+    }
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= $(document).height() - $(document).height() + 150) {
+            $('header.home').addClass('scroll-event')
+        } else {
+            $('header.home').removeClass('scroll-event')
+        }
     })
 
 })
